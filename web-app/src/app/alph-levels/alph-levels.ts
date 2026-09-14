18c6b725b4fff20690bc5fd2c1353f47bdca1c5d
Dragana Savić <dsavic@student.unipu.hr>
2026-09-14 21:33:22 +0200

    highlight dovršene razine

--- web-app/src/app/alph-levels/alph-levels.ts
+++ web-app/src/app/alph-levels/alph-levels.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DataAccessService } from '../data-access-service';
+import { GameConstants } from '../game-level-common/game-constants';
 
 @Component({
   imports: [RouterLink],
@@ -7,14 +9,25 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   styleUrl: './alph-levels.scss',
   templateUrl: './alph-levels.html',
 })
-export class AlphLevels {
+export class AlphLevels implements OnInit{
   playerId = "";
+  highestLevel = signal(0);
   private activatedRoute = inject(ActivatedRoute);
+  private dataAccess = inject(DataAccessService);
 
   constructor() {
     // Access route parameters
     this.activatedRoute.params.subscribe((params) => {
       this.playerId = params['playerId'];
     });
+  }
+
+  async ngOnInit(): Promise<void> {
+    const sessions = await this.dataAccess.getSessions(this.playerId);
+    const levels = sessions
+      .filter(x => x.chapter == GameConstants.alphChapter)
+      .map(x => x.level);
+    const highestLevel = Math.max(...levels);
+    this.highestLevel.set(highestLevel);
   }
 }
--- web-app/src/app/math-levels/math-levels.ts
+++ web-app/src/app/math-levels/math-levels.ts
@@ -1,5 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal, viewChild } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DataAccessService } from '../data-access-service';
+import { GameConstants } from '../game-level-common/game-constants';
 
 @Component({
   imports: [RouterLink],
@@ -7,14 +9,25 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
   styleUrl: './math-levels.scss',
   templateUrl: './math-levels.html',
 })
-export class MathLevels {
+export class MathLevels implements OnInit{
   playerId = "";
+  highestLevel = signal(0);
   private activatedRoute = inject(ActivatedRoute);
+  private dataAccess = inject(DataAccessService);
 
   constructor() {
     // Access route parameters
     this.activatedRoute.params.subscribe((params) => {
       this.playerId = params['playerId'];
     });
+  }
+  
+  async ngOnInit(): Promise<void> {
+    const sessions = await this.dataAccess.getSessions(this.playerId);
+    const levels = sessions
+      .filter(x => x.chapter == GameConstants.mathChapter)
+      .map(x => x.level);
+    const highestLevel = Math.max(...levels);
+    this.highestLevel.set(highestLevel);    
   }
 }
