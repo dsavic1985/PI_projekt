2475a5556930076523afc2171cefcc1240f18556
Dragana Savić <dsavic@student.unipu.hr>
2026-09-14 21:35:11 +0200

    firebase konfiguracija

--- web-app/src/environments/environment.development.ts
+++ web-app/src/environments/environment.development.ts
@@ -1,4 +1,12 @@
 export const environment = {    
   development: true,
   functionsBaseUrl: "http://127.0.0.1:5001/pi-projekt-36c14/us-central1",
+  firebase: {
+    apiKey: "-----",
+    authDomain: "-----",
+    projectId: "-----",
+    storageBucket: "-----",
+    messagingSenderId: "-----",
+    appId: "-----",
+  }
 };
--- web-app/src/environments/environment.ts
+++ web-app/src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
   development: false,
   functionsBaseUrl: "----",
+  firebase: {
+    apiKey: "AIzaSyDztTa1ItWWaU2NMpgRijuvpcAv7pq0DjU",
+    authDomain: "pi-projekt-36c14.firebaseapp.com",
+    projectId: "pi-projekt-36c14",
+    storageBucket: "pi-projekt-36c14.firebasestorage.app",
+    messagingSenderId: "971922687044",
+    appId: "1:971922687044:web:c4818fb77c86364d7be811"
+  }
 };
